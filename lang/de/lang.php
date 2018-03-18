edea3b7ac14a8596cf3dedfe9def310b50189a72
Frank Schiebel <frank@linuxmuster.net>
2018-03-18 18:39:20 +0100

    Removed wrong character

--- lang/de/lang.php
+++ lang/de/lang.php
@@ -1,4 +1,4 @@
 <?php
 
-$lang['js']['button'] = 'FÃge einen Link ein, um einen Unternamensraum zu erstellen';
-$lang['js']['prompt'] = 'Bitte geben Sie den Namen des Unternamensraums hier ein:';
+$lang['js']['button'] = 'Erstelle einen Link, um einen Unternamensraum zu erstellen';
+$lang['js']['prompt'] = 'Welchen Namen soll der Unternamensraums bekommen?';
